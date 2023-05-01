4fda1060b66022b9330d9aadebb833a09b222eaf
GeronimoReynoso <reynoso.geronimo@gmail.com>
2023-05-01 11:38:37 -0300

    agregado de rutas al archivo app.js

--- app.js
+++ app.js
@@ -10,5 +10,22 @@ app.listen(3000, () => {
 });
 
 app.get("/", (req, res) => {
-  res.sendFile(path.resolve(__dirname, `./views/home.html`));
+  res.sendFile(path.resolve(__dirname, `./views/index.html`));
 });
+
+app.get("/product", (req, res) => {
+  res.sendFile(path.resolve(__dirname, `./views/productDetail.html`));
+});
+
+app.get("/cart", (req, res) => {
+  res.sendFile(path.resolve(__dirname, `./views/productCart.html`));
+});
+
+app.get("/register", (req, res) => {
+  res.sendFile(path.resolve(__dirname, `./views/register.html`));
+});
+
+app.get("/login", (req, res) => {
+  res.sendFile(path.resolve(__dirname, `./views/login.html`));
+});
+
