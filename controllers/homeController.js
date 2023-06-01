5f778d9bf8c1125b51d315d859f344d54055691a
GeronimoReynoso <reynoso.geronimo@gmail.com>
2023-05-31 21:07:52 -0300

    transforme la lista de productos en un JSON

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,31 +1,10 @@
-const productos = [
-    {
-        nombre: "TOTE BAG",
-        imagen: "/images/mockups-productos/Mockup_Bolsa_Algodon_mockupgratis.com.jpg"
-    },
-    {
-        nombre: "THEME REMERA",
-        imagen: "/images/mockups-productos/Mockup_Camiseta_Doblada_mockupgratis.com.jpg"
-    },
-    {
-        nombre: "ANTEOJOS DE SOL",
-        imagen: "/images/mockups-productos/Mockup_Estuche_Gafas_Sol_mockupgratis.com.jpg"
-    },
-    {
-        nombre: "GORRO DE LANA",
-        imagen: "/images/mockups-productos/Mockup_Gorro_Lana_mockupgratis.com.jpg"
-    },
-    {
-        nombre: "BUZO",
-        imagen: "/images/mockups-productos/Mockup_Hoodie_Sudadera_mockupgratis.com.jpg"
-    },
-    {
-        nombre: "BARBIJO",
-        imagen: "/images/mockups-productos/Mockup_mascarilla_logo_mockupgratis.com.jpg"
-    }
-]
+const fs = require('fs');
+
+const productos = JSON.parse(fs.readFileSync('./productos.json')
 
 
+
+)
 
 const homeController = {
     list: function (req, res) {
