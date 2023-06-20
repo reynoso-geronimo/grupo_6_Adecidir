d201b7f4ff45bc8a2c7f58a9f61a36dae3a679b5
GeronimoReynoso <reynoso.geronimo@gmail.com>
2023-06-19 21:02:07 -0300

    logDBMiddleware solo funcione si no hay errores

--- src/middlewares/logDBMiddleware.js
+++ src/middlewares/logDBMiddleware.js
@@ -1,7 +1,11 @@
 const fs = require('fs');
+const { validationResult } = require('express-validator')
 
 module.exports = {
     logEdit:(req, res, next) => {
-    fs.appendFileSync('log.txt',new Date + ' Se edito el resgistro id: '+ req.params.id + "\n")
+    let errors = validationResult(req)
+    if(errors.isEmpty()){
+        fs.appendFileSync('log.txt',new Date + ' Se edito el resgistro id: '+ req.params.id + "\n")
+    }
     next()}
 }
