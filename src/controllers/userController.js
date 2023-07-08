6f7c02b16d4fac758b5600ab7ac299c211cbdd9b
GeronimoReynoso <reynoso.geronimo@gmail.com>
2023-07-08 17:09:27 -0300

    fixes(login)

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,103 +1,91 @@
-const fs = require("fs");
-const path = require("path");
 const { validationResult } = require('express-validator')
 const bcryptjs = require('bcryptjs')
 const User = require('../models/User.js')
 
-const usuarios = JSON.parse(fs.readFileSync(path.resolve(__dirname, "../database/usuarios.json"), "utf-8"));
+
 
 
 
 module.exports = {
-    loginForm: function (req, res) {
+    loginForm: function (req, res) { return res.render('user/login') },
 
-        return res.render('user/login')
+    loginProcess: function (req, res) {
+        const usuario = User.findByField('email', req.body.email)
+        if (usuario) {
+            bcryptjs.compare(req.body.password, usuario.clave)
+                .then(resultado => {
+                    if (resultado) {
+                        delete usuario.clave
+                        req.session.usuarioLogeado = usuario
+                        if (req.body.cookie) {
+                            res.cookie("recordarUsuario", req.body.email, { maxAge: 1000 * 60 * 60 })
+                        }
+                        return res.redirect("/")
+                    } else {
+                        console.log('hi');
+                        return res.render("user/login", {
+                            errors: {
+                                datosIncorrectos: {
+                                    msg: "Datos Incorrectos"
+                                }
+                            }
+                        })
+                    }
+                })
+                .catch(error => {
+                    console.log(error)
+                    return res.render("user/login", {
+                        errors: {
+                            datosIncorrectos: {
+                                msg: "Ocurrio un Error"
+                            }
+                        }
+                    })
+                });
 
-    },
 
-    loginProcess: function(req, res) {
-       const usuario = usuarios.find((row) => row.email == req.body.email)
-       if (usuario) {
-          if (usuario.password == req.body.password){
-            delete usuario.password
-            req.session.usuarioLogeado = usuario
-            if (req.body.cookie){
-                res.cookie("recordarUsuario", req.body.email, {maxAge: 1000*60*60})
-            }
-            return res.redirect("/")
-          } else {
+        } else {
             return res.render("user/login", {
-                error: {
-                    datosIncorrectos : {
-                        msg: "Datos Incorrectos :("
+                errors: {
+                    datosIncorrectos: {
+                        msg: "Datos Incorrectos"
                     }
                 }
             })
-          }
-       }else {
-        return res.render("user/login", {
-            error: {
-                datosIncorrectos : {
-                    msg: "Datos Incorrectos :("
-                }
-            }
-        })
-      }
+        }
     },
 
 
 
-    registerForm:function(req,res) {
-
-        return res.render('user/register')
-
-    },
+    registerForm: function (req, res) { return res.render('user/register') },
     // Registro
-    processRegister : function (req, res){
-        let userInDb = User.findByField('email', req.body.email)
+    processRegister: function (req, res) {
+        const userInDb = User.findByField('email', req.body.email)
         // para no usar el mismo email, despues voy a hacer las validaciones asi sale el mensaje y todo
-        if(userInDb){
+        if (userInDb) {
             return res.render('user/register',
-            { errors : {
-                email : {
-                    msg: 'este email ya esta en uso'
-                },
-                oldData : req.body
+                {
+                    errors: {
+                        email: {
+                            msg: 'este email ya esta en uso'
+                        },
+                        oldData: req.body
+                    }
 
-            }
-              
-            });        
+                });
         }
 
-        let userToCreate = { 
+        const userToCreate = {
             ...req.body,
             clave: bcryptjs.hashSync(req.body.clave, 10),
-            
         }
         userToCreate.categoria = "customer";
         userToCreate.avatar = req.file.filename
         delete userToCreate.passwordRepeat
-        let userCreated = User.create(userToCreate)
+        User.create(userToCreate)
 
         return res.redirect('login')
     }
 
 }
 
-/* VER SI EN UN FUTURO SE LE PUEDE DAR FUNCIONALIDAD PARA ALGO
-
-const usuarios = JSON.parse(fs.readFileSync(path.resolve(__dirname, "../database/usuarios.json"), "utf-8"));
-
-const userDetail = {
-    loginForm: function (req, res) {
-        const usuarios = usuarios.find((row) => row.id == req.params.id);
-        return res.render('user/login', { usuarios: usuarios, categorias: categorias });
-    },
-    registerForm:function(req,res) {
-        const usuarios = usuarios.find((row) => row.id == req.params.id);
-        return res.render('user/register', { usuarios: usuarios, categorias: categorias });
-
-    }
-}
-
-  module.exports = userDetail; */
