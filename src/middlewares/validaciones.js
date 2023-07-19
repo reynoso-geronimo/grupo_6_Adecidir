7d5e5f073a5937faf8651ccd5b018c5bd82322be
Reynoso <reynoso.geronimo@gmail.com>
2023-07-19 14:39:12 -0300

    sanitizacion de inputs

--- src/middlewares/validaciones.js
+++ src/middlewares/validaciones.js
@@ -2,15 +2,15 @@ const { body } = require('express-validator')
 
 module.exports ={
     validarEdit : [
-        body('name').notEmpty().withMessage('Debes completar el campo de nombre'),
-        body('price').notEmpty().withMessage('Debes completar el campo de precio con un valor valido').bail().isFloat({gt:0}).withMessage('Debes completar el campo de precio con un valor valido'),
-        body('category').notEmpty().withMessage('Debes completar el campo de categoria'),
-        body('desc').notEmpty().withMessage('Debes completar el campo de descripcion'),
-        body('size[0]').notEmpty().withMessage('Debes completar el campo de talle S con un valor valido').bail().isInt({ gt: -1 }).withMessage('Debes completar el campo de talle S con un valor valido'),
-        body('size[1]').notEmpty().withMessage('Debes completar el campo de talle M con un valor valido').bail().isInt({ gt: -1 }).withMessage('Debes completar el campo de talle M con un valor valido'),
-        body('size[2]').notEmpty().withMessage('Debes completar el campo de talle L con un valor valido').bail().isInt({ gt: -1 }).withMessage('Debes completar el campo de talle L con un valor valido'),
-        body('size[3]').notEmpty().withMessage('Debes completar el campo de talle XL con un valor valido').bail().isInt({ gt: -1 }).withMessage('Debes completar el campo de talle XL con un valor valido'),
-        body('size[4]').notEmpty().withMessage('Debes completar el campo de talle XXL con un valor valido').bail().isInt({ gt: -1 }).withMessage('Debes completar el campo de talle XXL con un valor valido'),
+        body('name').notEmpty().withMessage('Debes completar el campo de nombre').trim().escape(),
+        body('price').notEmpty().withMessage('Debes completar el campo de precio con un valor valido').bail().isFloat({gt:0}).withMessage('Debes completar el campo de precio con un valor valido').trim().escape(),
+        body('category').notEmpty().withMessage('Debes completar el campo de categoria').trim().escape(),
+        body('desc').notEmpty().withMessage('Debes completar el campo de descripcion').escape().trim(),
+        body('size[0]').notEmpty().withMessage('Debes completar el campo de talle S con un valor valido').bail().isInt({ gt: -1 }).withMessage('Debes completar el campo de talle S con un valor valido').trim().escape(),
+        body('size[1]').notEmpty().withMessage('Debes completar el campo de talle M con un valor valido').bail().isInt({ gt: -1 }).withMessage('Debes completar el campo de talle M con un valor valido').trim().escape(),
+        body('size[2]').notEmpty().withMessage('Debes completar el campo de talle L con un valor valido').bail().isInt({ gt: -1 }).withMessage('Debes completar el campo de talle L con un valor valido').trim().escape(),
+        body('size[3]').notEmpty().withMessage('Debes completar el campo de talle XL con un valor valido').bail().isInt({ gt: -1 }).withMessage('Debes completar el campo de talle XL con un valor valido').trim().escape(),
+        body('size[4]').notEmpty().withMessage('Debes completar el campo de talle XXL con un valor valido').bail().isInt({ gt: -1 }).withMessage('Debes completar el campo de talle XXL con un valor valido').trim().escape(),
       
       ]
 }
