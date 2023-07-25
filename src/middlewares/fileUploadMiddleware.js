891ab1329dc26754f0361530444865f32dae6ad3
Reynoso <reynoso.geronimo@gmail.com>
2023-07-25 10:30:51 -0300

    movi multer fuera del archivo de ruta de usuarios

--- src/middlewares/fileUploadMiddleware.js
+++ src/middlewares/fileUploadMiddleware.js
@@ -2,22 +2,22 @@
 const path = require('path');
 const multer = require('multer');
 
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-  
-      cb(null, path.resolve(__dirname, '../../public/images/productos'))
-    },
-    filename: function (req, file, cb) {
-      const uniqueSuffix = Date.now() + Math.round(Math.random() * 1E9)
-  
-      cb(null, uniqueSuffix + path.extname(file.originalname))
-    }
-  })
+
 
 module.exports = {
 
     uploadImgProducto:multer({
-        storage: storage,
+        storage: multer.diskStorage({
+          destination: function (req, file, cb) {
+        
+            cb(null, path.resolve(__dirname, '../../public/images/productos'))
+          },
+          filename: function (req, file, cb) {
+            const uniqueSuffix = Date.now() + Math.round(Math.random() * 1E9)
+        
+            cb(null, uniqueSuffix + path.extname(file.originalname))
+          }
+        }),
         limits:{fileSize:1024*1024*10},
         fileFilter: (req, file, cb) => {
           if (file.mimetype.includes('image')) {
@@ -30,6 +30,28 @@ module.exports = {
           }
         }
       }),
-   
+      uploadImgAvatar:multer({
+        storage: multer.diskStorage({
+          destination: function (req, file, cb) {
+              cb(null, path.resolve(__dirname, '../../public/images/avatar'))
+          },
+          filename: function (req, file, cb) {
+            const uniqueSuffix = Date.now() + Math.round(Math.random() * 1E9)
+        
+            cb(null, uniqueSuffix + path.extname(file.originalname))
+          }
+        }),
+        limits:{fileSize:1024*1024*10},
+        fileFilter: (req, file, cb) => {
+          if (file.mimetype.includes('image')) {
+            cb(null, true);
+          } else {
+      
+            req.fileValidationError = "Solo imagenes estan permitidas";
+            return cb(null, false);
+      
+          }
+        }
+      }),
 }
 
--- src/routes/user.js
+++ src/routes/user.js
@@ -4,32 +4,12 @@ const path = require('path');
 const multer = require('multer')
 const validateAvatar = require('../middlewares/avatarMiddleware.js')
 const {logged, userAcess,adminAcces} = require('../middlewares/authMiddleware.js');
+const {uploadImgAvatar} = require('../middlewares/fileUploadMiddleware.js')
 const userController = require('../controllers/userController.js');
 
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-      cb(null, path.resolve(__dirname, '../../public/images/avatar'))
-  },
-  filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now() + Math.round(Math.random() * 1E9)
 
-    cb(null, uniqueSuffix + path.extname(file.originalname))
-  }
-});
-const upload = multer({
-  storage: storage,
-  fileFilter: (req, file, cb) => {
-    if (file.mimetype == "image/png" || file.mimetype == "image/jpg" || file.mimetype == "image/jpeg") {
-      cb(null, true);
-    } else {
 
-      req.fileValidationError = "Solo formato png, jpg o jpeg estan permitidos";
-      return cb(null, false, req.fileValidationError);
-
-    }
-  }
-})
 
 
 // login
@@ -39,7 +19,7 @@ router.post("/login", logged,userController.loginProcess);
 
 // register
 router.get("/register", logged,userController.registerForm);
-router.post("/register", logged,upload.single('avatar'),validateAvatar,userController.processRegister);
+router.post("/register", logged,uploadImgAvatar.single('avatar'),validateAvatar,userController.processRegister);
 
 
 router.get("/profile", userAcess,userController.perfil);
