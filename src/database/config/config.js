b6fe1f4424e6b49001d16f9521368f7b9620047a
Reynoso <reynoso.geronimo@gmail.com>
2023-08-04 13:51:26 -0300

    reinicialize sequelize

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,9 +1,11 @@
+require("dotenv").config()
+
 module.exports ={
   "development": {
-    "username": "DB_USER",
-    "password": DB_PASS,
-    "database": "DB_NAME",
-    "host": "127.0.0.1",
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASS,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST,
     "dialect": "mysql"
   },
   "test": {
