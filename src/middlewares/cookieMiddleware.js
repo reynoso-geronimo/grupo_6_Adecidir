f8240530d81a86b889051d37c1799fff5d3bbc06
Reynoso <reynoso.geronimo@gmail.com>
2023-08-11 09:06:41 -0300

    implementado try catch

--- src/middlewares/cookieMiddleware.js
+++ src/middlewares/cookieMiddleware.js
@@ -2,9 +2,13 @@ const db = require('../database/models')
 
 const cookieUsuario = async(req, res, next) => {
     if (!req.session.usuarioLogeado && req.cookies.recordarUsuario){
+       try {
         const usuario = await db.Usuarios.findByPk(req.cookies.recordarUsuario) 
         delete usuario.password
             req.session.usuarioLogeado = usuario   
+       } catch (error) {
+        console.log("ocurrio un error al login con cookie");
+       }
     }
     next()
 }
