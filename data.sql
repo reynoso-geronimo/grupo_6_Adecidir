6b9d442fa9365784fb3363be76ae6a829d6c3a94
Reynoso <reynoso.geronimo@gmail.com>
2023-08-14 16:20:50 -0300

    mas categorias

--- data.sql
+++ data.sql
@@ -9,8 +9,17 @@ VALUES('aschankelborg7@sciencedaily.com','Maria-Fernanda','Cabello','$2a$10$UoO.
 
 
 INSERT INTO theme_db.Categorias(nombre,imagen)
-VALUES('pantalones','pantalones.jpg');
+VALUES
+('PANTALONES','pantalones.jpg'),
+('REMERAS','Mockup_Camiseta_Doblada_mockupgratis.com.jpg'),
+('ABRIGOS','Mockup_Hoodie_Sudadera_mockupgratis.com.jpg'),
+('COMPLEMENTOS','Mockup_Bolsa_Tela_Chica_mockupgratis.com.jpg'),
+('GIFT CARD','Tarjeta_Presentacion_mockupgratis.com.jpg');
+
+
 INSERT INTO theme_db.Productos(nombre,descripcion,id_categoria,precio,talleXS,talleS,talleM,talleL,talleXL,talleXXL,talleUnico)
 VALUES('jeans','pantalon largo negro',1,400,3,4,5,6,7,3,5);
+
+
 INSERT INTO theme_db.Tickets(fecha,usuario_id)VALUES('2023-08-02',1);
 INSERT INTO theme_db.Productos_tickets(id_producto,precioFechaCompra,id_ticket,cantidad)VALUES(2,'2023-08-02',2,1);
