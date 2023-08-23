8eee7cdff518a8f6a9b1825e74a9c5f80665386a
Reynoso <reynoso.geronimo@gmail.com>
2023-08-23 14:55:28 -0300

    logica de front para talles

---
+++ public/js/productEditCreateTalle.js
@@ -0,0 +1,20 @@
+window.addEventListener("load",()=>{
+    let talle = document.querySelector(".editar-talle")
+    let switchTalle = document.querySelector("#switchTalle")
+    switchTalle.addEventListener("change",(e)=>{
+        if(e.currentTarget.checked){
+            talle.innerHTML=`
+            <label for="talles">Unidades <input type="number" id="talleUnico" name="talleunico" value="0" /></label>
+            `
+        }else{
+            talle.innerHTML=`
+            <label for="talles">S <input type="number" id="talleS" name="talles" value="0" /></label>
+            <label for="tallem">M <input type="number" id="talleM" name="tallem" value="0" /></label>
+            <label for="tallel">L <input type="number" id="talleL" name="tallel" value="0" /></label>
+            <label for="tallexl">XL <input type="number" id="talleXL" name="tallexl" value="0" /></label>
+            <label for="tallexxl">XXL <input type="number" id="talleXXL" name="tallexxl" value="0" /></label>
+            `
+        }
+    })
+   
+})
