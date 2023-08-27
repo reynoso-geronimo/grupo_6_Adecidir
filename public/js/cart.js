49c6424d4eca9df232891d01546f8439eb663a46
GeronimoReynoso <reynoso.geronimo@gmail.com>
2023-08-27 19:18:02 -0300

    ya se pueden eliminar items del carrito

--- public/js/cart.js
+++ public/js/cart.js
@@ -10,21 +10,19 @@ window.addEventListener('load',()=>{
 
     let carrito =  document.querySelector("#productos")
     let checkout =  document.querySelector(".checkout")
+    let cantidadItems =  document.querySelector("#cantidadCarrito")
 
     const consultarProductosDB = async function(productos){
       const resultado= await fetch('/product/cartitems',{
         method:'POST',
-        headers: {
-          'Content-Type': 'application/json'
-          
-        },
+        headers: {'Content-Type': 'application/json'},
         body: JSON.stringify(productos)
       })
       return resultado.json()
     }
-
+    
     const generarCarrito = async function(items){
-     
+
        if(carritoStorage.length==0){
           
           carrito.innerHTML += `<h1>No hay items en el carrito </h1>`
@@ -33,7 +31,9 @@ window.addEventListener('load',()=>{
         const consultaDb = await consultarProductosDB(items)
         carrito.innerHTML = ``
         checkout.innerHTML = ``
+        cantidadItems.innerText = `Carrito(${consultaDb.length})`
         let total = 0
+        let lugarEnArray = 0
         for (const item of consultaDb) {
             total += (item.precio*item.cantidad)
             carrito.innerHTML+= `
@@ -48,18 +48,39 @@ window.addEventListener('load',()=>{
                 <p>Unidades: ${item.cantidad}</p>
                 <p>Precio: $${item.precio*item.cantidad}</p>
               </div>
-              <div><a href="#">Guardar</a><i class="fa-solid fa-trash"></i></div>
+              <div><i id="${lugarEnArray}" class="fa-solid fa-trash"></i></div>
             </div>
           </article>
             `
-
+          lugarEnArray++
         }
+        
         checkout.innerHTML = `
         <p>Total: $ ${total}</p>
         <button class="boton-negro invertido">Seguir Comprando</button>
         <button class="boton-negro">Comprar Carrito</button>`
        }
-    
+  
+      cargarEliminar()
     }
+    
     generarCarrito(carritoStorage)
+
+    function cargarEliminar() {
+      const botonEliminar = document.querySelectorAll('.fa-trash');
+
+      botonEliminar.forEach((boton, index) => {
+          boton.addEventListener('click', () => {
+              
+              carritoStorage.splice(index, 1);
+              localStorage.setItem('cart', JSON.stringify(carritoStorage)); 
+              generarCarrito(carritoStorage); 
+          });
+      });
+  }
+
+  cargarEliminar();
+    
 })
+
+
