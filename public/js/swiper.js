27185c0eb440b1595725ee3cc53373c5d8be9fc9
Ignacio Furfaro <furfaroignacio@gmail.com>
2023-08-30 14:37:10 -0300

    swiper

---
+++ public/js/swiper.js
@@ -0,0 +1,30 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    effect : "fade",
+    autoHeight : true,
+    autoplay : {
+        delay : 3000,
+        pauseOnMouseEnter : true,
+        disableOnInteraction: false 
+    },
+
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable : true,
+      dynamicBullets:true, 
+    },
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    // And if we need scrollbar
+    /* scrollbar: {
+      el: '.swiper-scrollbar',
+    }, */
+  });
