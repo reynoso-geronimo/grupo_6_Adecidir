4b4794a228b4856ac3a74e8fb263475f9521c188
GeronimoReynoso <reynoso.geronimo@gmail.com>
2023-09-01 17:57:53 -0300

    un poco de interaccion

--- public/js/productDetail.js
+++ public/js/productDetail.js
@@ -20,41 +20,57 @@ window.addEventListener("load", function () {
   });
 
   // agregar items al carrito
-  let carritoStorage = []
+  let carritoStorage = [];
   const carritoInit = function () {
-
     if (!JSON.parse(localStorage.getItem("cart"))) {
       localStorage.setItem("cart", JSON.stringify(carritoStorage));
     } else {
       carritoStorage = JSON.parse(localStorage.getItem("cart"));
     }
   };
-  carritoInit()
+  carritoInit();
 
-  const addCarrito = document.querySelector("#addCart")
+  const addCarrito = document.querySelector("#addCart");
   addCarrito.addEventListener("click", () => {
     const selectedTalle = document.querySelector('input[name="talle"]:checked');
     const idRegExp = /\/product\/(\d+)/;
     const id = window.location.href.match(idRegExp);
     if (selectedTalle) {
-
       const producto = {
         id: id[1],
         talle: selectedTalle.value,
-        cantidad: 1
-      }
-      const productoEnCarrito = carritoStorage.find(itemsEnCarrito => itemsEnCarrito.id === producto.id && itemsEnCarrito.talle === producto.talle)
-      
-      if (productoEnCarrito) {
-         productoEnCarrito.cantidad += producto.cantidad 
-        
-        }
-      
-      else { carritoStorage.push(producto) }
-      localStorage.setItem("cart", JSON.stringify(carritoStorage));
-    } else {
-      addCarrito.innerText = "Elige un talle"
-    }
-  })
+        cantidad: 1,
+      };
+      const productoEnCarrito = carritoStorage.find(
+        itemsEnCarrito =>
+          itemsEnCarrito.id === producto.id &&
+          itemsEnCarrito.talle === producto.talle
+      );
 
+      if (productoEnCarrito) {
+        productoEnCarrito.cantidad += producto.cantidad;
+      } else {
+        carritoStorage.push(producto);
+      }
+      localStorage.setItem("cart", JSON.stringify(carritoStorage));
+      Swal.fire({
+        title: "<strong>Producto agregado al carrito</strong>",
+        // icon: "info",
+        html:
+          // "You can use <b>bold text</b>, " +
+          // '<a href="//sweetalert2.github.io">links</a> ' +
+          // "and other HTML tags",
+          "",
+        showCloseButton: false,
+        showCancelButton: true,
+        focusConfirm: true,
+        confirmButtonText: '<a href="/user/cart">Finalizar compra</a>',
+        confirmButtonAriaLabel: "Thumbs up, great!",
+        cancelButtonText: '<a href="/">Seguir comprando</a>',
+        cancelButtonAriaLabel: "Thumbs down",
+      });
+    } else {
+      addCarrito.innerText = "Elige un talle";
+    }
+  });
 });
