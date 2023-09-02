2781a2173cec3de7bc377fcf7d3e146a16e2607b
GeronimoReynoso <reynoso.geronimo@gmail.com>
2023-09-02 16:45:59 -0300

    fixes validaciones de back

--- src/middlewares/validaciones.js
+++ src/middlewares/validaciones.js
@@ -3,16 +3,17 @@ const { check } = require('express-validator')
 module.exports ={
     validarImagenSize:(err,req,res,next)=>{if(err.code== 'LIMIT_FILE_SIZE'){req.fileValidationError = "Limite 10 MB";}next()},
     validarEditProduct : [
-        check('nombre').notEmpty().withMessage('Debes completar el campo de nombre').isLength({min : 5}).withMessage('El nombre del producto debe tener al menos 5 caracteres').trim().escape(),
+        check('nombre').notEmpty().withMessage('Debes completar el campo de nombre').bail().isLength({min : 5}).withMessage('El nombre del producto debe tener al menos 5 caracteres').trim().escape(),
         check('precio').notEmpty().withMessage('Debes completar el campo de precio con un valor valido').bail().isFloat({gt:0}).withMessage('Debes completar el campo de precio con un valor valido').trim().escape(),
         check('id_categoria').notEmpty().withMessage('Debes completar el campo de categoria').trim().escape(),
-        check('descripcion').notEmpty().withMessage('Debes completar el campo de descripcion').isLength({ min: 20 }).withMessage('La descripción debe tener al menos 20 caracteres.').escape().trim(),
-        check('talleUnico').if(( { req }) => !value).isInt({ min: 0 }).withMessage('El talle debe ser entero no negativo.').trim().escape(),
-        check('talles').if(( { req }) => !req.body.talleUnico).isInt({ min: 0 }).withMessage('Los talles deben ser enteros no negativos.').trim().escape(),
-        check('tallem').if(( { req }) => !req.body.talleUnico).isInt({ min: 0 }).withMessage('Los talles deben ser enteros no negativos.').trim().escape(),
-        check('tallel').if(( { req }) => !req.body.talleUnico).isInt({ min: 0 }).withMessage('Los talles deben ser enteros no negativos.').trim().escape(),
-        check('tallexl').if(( { req }) => !req.body.talleUnico).isInt({ min: 0 }).withMessage('Los talles deben ser enteros no negativos.').trim().escape(),
-        check('tallexxl').if(( { req }) => !req.body.talleUnico).isInt({ min: 0 }).withMessage('Los talles deben ser enteros no negativos.').trim().escape(),
+        check('descripcion').notEmpty().withMessage('Debes completar el campo de descripcion').bail().isLength({ min: 20 }).withMessage('La descripción debe tener al menos 20 caracteres.').escape().trim(),
+        check('talleUnico').if((value, { req }) => req.body.talleUnico).isInt({ min: 1 }).withMessage('El talle debe ser entero no negativo.').trim().escape(),
+        check('talleUnico').if((value, { req }) => !req.body.talleUnico&&req.body.talles+req.body.tallem+req.body.tallel+req.body.tallexl+req.body.tallexxl==0).isInt({ min: 1 }).withMessage('Debes cargar un talle').trim().escape(),
+        check('talles').if((value, { req }) => !req.body.talleUnico).isInt({ min: 0 }).withMessage('Los talles deben ser enteros no negativos.').trim().escape(),
+        check('tallem').if((value, { req }) => !req.body.talleUnico).isInt({ min: 0 }).withMessage('Los talles deben ser enteros no negativos.').trim().escape(),
+        check('tallel').if((value, { req }) => !req.body.talleUnico).isInt({ min: 0 }).withMessage('Los talles deben ser enteros no negativos.').trim().escape(),
+        check('tallexl').if(( value,{ req }) => !req.body.talleUnico).isInt({ min: 0 }).withMessage('Los talles deben ser enteros no negativos.').trim().escape(),
+        check('tallexxl').if(( value,{ req }) => !req.body.talleUnico).isInt({ min: 0 }).withMessage('Los talles deben ser enteros no negativos.').trim().escape(),
         //TODO validar campo talle unico
         check('imagenes').custom((value,{req})=>{
           if(req.fileValidationError){
@@ -22,24 +23,17 @@ module.exports ={
         })
       ],
       validarCrearProducto:[
-        check('nombre')
-        .notEmpty().withMessage('El nombre del producto es obligatorio.')
-        .isLength({ min: 5 }).withMessage('El nombre debe tener al menos 5 caracteres.')
-        .trim()
-        .escape(),
-        check('precio').notEmpty().withMessage('El precio del producto es obligatorio.').isNumeric().withMessage('El precio debe ser un valor numérico.').trim().escape(),
+        check('nombre').notEmpty().withMessage('El nombre del producto es obligatorio.').bail().isLength({ min: 5 }).withMessage('El nombre debe tener al menos 5 caracteres.').trim().escape(),
+        check('precio').notEmpty().withMessage('El precio del producto es obligatorio.').bail().isNumeric().withMessage('El precio debe ser un valor numérico.').trim().escape(),
         check('id_categoria').notEmpty().withMessage('Debes seleccionar una categoría.').trim().escape(),
-        check('talleUnico').if((value, { req }) => !value).isInt({ min: 0 }).withMessage('El talle debe ser entero no negativo.').trim().escape(),
+        check('talleUnico').if((value, { req }) => req.body.talleUnico).isInt({ min: 1 }).withMessage('El talle debe ser entero no negativo.').trim().escape(),
+        check('talleUnico').if((value, { req }) => !req.body.talleUnico&&req.body.talles+req.body.tallem+req.body.tallel+req.body.tallexl+req.body.tallexxl==0).isInt({ min: 1 }).withMessage('Debes cargar un talle').trim().escape(),
         check('talles').if((value, { req }) => !req.body.talleUnico).isInt({ min: 0 }).withMessage('Los talles deben ser enteros no negativos.').trim().escape(),
         check('tallem').if((value, { req }) => !req.body.talleUnico).isInt({ min: 0 }).withMessage('Los talles deben ser enteros no negativos.').trim().escape(),
         check('tallel').if((value, { req }) => !req.body.talleUnico).isInt({ min: 0 }).withMessage('Los talles deben ser enteros no negativos.').trim().escape(),
         check('tallexl').if((value, { req }) => !req.body.talleUnico).isInt({ min: 0 }).withMessage('Los talles deben ser enteros no negativos.').trim().escape(),
         check('tallexxl').if((value, { req }) => !req.body.talleUnico).isInt({ min: 0 }).withMessage('Los talles deben ser enteros no negativos.').trim().escape(),
-        check('descripcion')
-        .notEmpty().withMessage('La descripción del producto es obligatoria.')
-        .isLength({ min: 20 }).withMessage('La descripción debe tener al menos 20 caracteres.')
-        .trim()
-        .escape(),
+        check('descripcion').notEmpty().withMessage('La descripción del producto es obligatoria.').bail().isLength({ min: 20 }).withMessage('La descripción debe tener al menos 20 caracteres.').trim().escape(),
         check('imagenes').custom((value, { req }) => {
           if (!req.files || req.files.length === 0) {
             throw new Error('Debes subir al menos una imagen del producto.');
