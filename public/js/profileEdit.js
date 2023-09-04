91f1d506c478db4b4b56cfd16ef8de1f241cc3ee
GeronimoReynoso <reynoso.geronimo@gmail.com>
2023-09-04 19:37:25 -0300

    validacion de edicion de usuario

--- public/js/profileEdit.js
+++ public/js/profileEdit.js
@@ -1,33 +1,39 @@
-let formularioEdicionPerfil = document.querySelector("#formularioEdicionPerfil");
-formularioEdicionPerfil.onsubmit= ()=>{
-    let nombre = document.querySelector("input.nombre");
-    let apellido = document.querySelector("input.apellido");
-    let direccion = document.querySelector("input.direccion");
-    let telefono = document.querySelector("input.telefono");
+const formularioEdicionPerfil = document.querySelector("#formularioEdicionPerfil");
+formularioEdicionPerfil.onsubmit= (e)=>{
+    e.preventDefault()
+    const nombre = document.querySelector("#nombre");
+    const apellido = document.querySelector("#apellido");
+    const direccion = document.querySelector("#direccion");
+    const telefono = document.querySelector("#telefono");
+    let errores = 0 
+    
+    if(nombre.value.length <= 1 ){
+        errores ++
+        document.querySelector('#error-nombre').innerText="El nombre es un campo obligatorio." 
+    }else{
+        document.querySelector('#error-nombre').innerText=""
+    }
+    if(apellido.value.length <= 1 ){
+        errores ++
+        document.querySelector('#error-apellido').innerText="El apellido es un campo obligatorio." 
+    }else{
+        document.querySelector('#error-apellido').innerText=""
+    }
+   
+    if(direccion.value.length <= 1 ){
+        errores ++
+        document.querySelector('#error-direccion').innerText="La direccion es un campo obligatorio." 
+    }else{
+        document.querySelector('#error-direccion').innerText=""
+    }
 
-    let errores = [];
-    if(nombre.length <= 1 ){
-        errores.push("El nombre es un campo obligatorio.");
-    }
-    if(apellido.length <= 1 ){
-        errores.push("El apellido es un campo obligatorio.");
-    }
-    if(direccion.length <= 1 ){
-        errores.push("El direccion es un campo obligatorio.");
-    }
-    if(telefono.length <= 1 ){
-        errores.push("El telefono es un campo obligatorio.");
-    }
-
-    if(errores.length > 0){
-        event.preventDefault();
-        let signIn = document.querySelector(".body-login .sign-in");
-        signIn+= '<div class="errores">';
-        errores.forEach(error=>{
-            signIn+= '<p>${error}</p>';
-        });
-        signIn+= '</div">';
-
+    if(telefono.value.length <= 1 ){
+        errores ++
+        document.querySelector('#error-telefono').innerText="El telefono es un campo obligatorio." 
+    }else{
+        document.querySelector('#error-telefono').innerText=""
     }
     
+    
+    errores===0?formularioEdicionPerfil.submit():null
 }
