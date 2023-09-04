282433882354fe76774bc00b28f5c31e60e2cc51
Reynoso <reynoso.geronimo@gmail.com>
2023-09-04 09:26:54 -0300

    fix pero queda horrible

--- public/js/register.js
+++ public/js/register.js
@@ -1,44 +1,47 @@
 let formularioRegistro = document.querySelector("#formularioRegistro");
-formularioRegistro.onsubmit= ()=>{
-    let nombre = document.querySelector("input.nombre");
-    let apellido = document.querySelector("input.apellido");
-    let email = document.querySelector("input.email");
-    let clave = document.querySelector("input.clave");
-    let passwordRepeat = document.querySelector("input.passwordRepeat");
+formularioRegistro.onsubmit= (event)=>{
+    event.preventDefault()
+    let nombre = document.querySelector("#nombre");
+    let apellido = document.querySelector("#apellido");
+    let email = document.querySelector("#email");
+    let clave = document.querySelector("#clave");
+    let passwordRepeat = document.querySelector("#passwordRepeat");
 
     let errores = [];
-    if(nombre.length <= 1 ){
+    if(nombre.value.length <= 1 ){
         errores.push("El nombre es un campo obligatorio.");
     }
-    if(apellido.length <= 1 ){
+    if(apellido.value.length <= 1 ){
         errores.push("El apellido es un campo obligatorio.");
     }
-    if(email.length < 8 || email.length > 255){
+    if(email.value.length < 8 || email.length > 255){
         errores.push("El Email esta fuera de rango, no tiene entre 8 y 255 caracteres");
     }
-    if(!email.includes("@") || !email.includes(".")){
+    if(!email.value.includes("@") || !email.includes(".")){
         errores.push("El Email no tiene el formato de correo electrónico (ejemplo: sara@gmail.com).");
 
     }
-    if(clave.length < 8 || clave.length > 255){
+    if(clave.value.length < 8 || clave.length > 255){
         errores.push("La primera contraseña esta fuera de rango, no tiene entre 8 y 255 caracteres");
     }
-    if(!passwordRepeat.length < 8 || !passwordRepeat.length > 255){
+    if(!passwordRepeat.value.length < 8 || !passwordRepeat.length > 255){
         errores.push("La la repeticion de Contraseña esta fuera de rango, no tiene entre 8 y 255 caracteres");
 
     }
-    if(clave != passwordRepeat){
+    if(clave.value != passwordRepeat.value){
         errores.push("Las contraseñas no coinciden.");
     }
 
     if(errores.length > 0){
-        event.preventDefault();
-        let signIn = document.querySelector(".body-login .sign-in");
-        signIn+= '<div class="errores">';
+        
+        let signIn = document.querySelector(".body-register");
+        console.log(signIn)
+        signIn.innerHTML+= '<div class="errores">';
         errores.forEach(error=>{
-            signIn+= '<p>${error}</p>';
+            console.log(error)
+            signIn.innerHTML+= `<p>${error}</p>`;
         });
-        signIn+= '</div">';
+        signIn.innerHTML+= '</div">';
 
     }
     
