098fb8d4f1163fb3353e1a1a71a09b358f54c746
GeronimoReynoso <reynoso.geronimo@gmail.com>
2023-09-04 19:26:55 -0300

    mas validaciones

--- public/js/profileEditPassword.js
+++ public/js/profileEditPassword.js
@@ -1,8 +1,9 @@
-const PwResetForm = document.querySelector("#PwResetForm");
-PwResetForm.onsubmit= (event)=>{
+const pwEdit = document.querySelector("#pwEdit");
+pwEdit.onsubmit= (event)=>{
     event.preventDefault()
    
     const clave = document.querySelector("#clave");
+    const passwordNueva = document.querySelector("#passwordNueva");
     const passwordRepeat = document.querySelector("#passwordRepeat");
     let errores = 0
 
@@ -12,14 +13,19 @@ PwResetForm.onsubmit= (event)=>{
     }else{
         document.querySelector('#error-clave').innerText=""
     }
-    
-    if(clave.value != passwordRepeat.value){
+    if(passwordNueva.value.length < 8 || passwordNueva.length > 255){
+        errores ++
+        document.querySelector('#error-claveNueva').innerText="Fuera de rango, no tiene entre 8 y 255 caracteres" 
+    }else{
+        document.querySelector('#error-claveNueva').innerText=""
+    }
+    if(passwordNueva.value != passwordRepeat.value){
         errores ++
         document.querySelector('#error-claveRepeat').innerText="Las contraseñas no coinciden." 
     }else{
         document.querySelector('#error-claveRepeat').innerText=""
     }
-    errores===0?PwResetForm.submit():null
+    errores===0?pwEdit.submit():null
     
     
 }
