aff4399cf305f38abf24dced0da190bc8f77f401
Reynoso <reynoso.geronimo@gmail.com>
2023-09-13 10:09:35 -0300

    fix

--- src/routes/apiRoutes.js
+++ src/routes/apiRoutes.js
@@ -2,11 +2,9 @@ const express = require('express');
 const router = express.Router()
 const apiController = require('../controllers/apiController.js');
 
-// productos
-router.get('/products/list/:categoria?', apiController.productList)
 
 //usuarios
-router.get('/users/list/', apiController.userList)
+router.get('/users/', apiController.userList)
 router.get('/users/:id', apiController.userDetail)
 
 // products
