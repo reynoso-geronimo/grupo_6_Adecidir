eabf4d36dd2f032085e4e8ba6cf837b439875173
Reynoso <reynoso.geronimo@gmail.com>
2023-09-13 09:18:31 -0300

    code cleanup

--- src/controllers/apiController.js
+++ src/controllers/apiController.js
@@ -1,6 +1,3 @@
-const fs = require("fs");
-const path = require("path");
-const { validationResult } = require("express-validator");
 const db = require("../database/models");
 const { Op } = require("sequelize");
 
